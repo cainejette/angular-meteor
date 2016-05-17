bd835ae143d6b2657933b8d95c0e852a4315163c
Caine Jette <caine@attackpattern.com>
2016-05-16 21:03:37 -0700

    Controller --> component

--- socially/client/main.js
+++ socially/client/main.js
@@ -2,16 +2,20 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
  
 angular.module('socially', [
-  angularMeteor
+    angularMeteor
   ])
-  .controller('PartiesListCtrl', function($scope, $reactive) {
-    'ngInject';
-    
-    $reactive(this).attach($scope);
-    
-    this.helpers({
-      parties() {
-        return Parties.find({});
-      }
-    })
+  .component('partiesList', {
+    templateUrl: 'client/partiesList.html',
+    controllerAs: 'partiesList',
+    controller($scope, $reactive) {
+      'ngInject';
+ 
+      $reactive(this).attach($scope);
+ 
+      this.helpers({
+        parties() {
+          return Parties.find({});
+        }
+      });
+    }
   });
